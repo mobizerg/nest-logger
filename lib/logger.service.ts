e5e1300fa29f3a7572be86d06b232f4a855f77de
Mobizerg <mobizerg@gmail.com>
2020-01-27 15:49:18 +0530

    Added context to meta args

--- lib/logger.service.ts
+++ lib/logger.service.ts
@@ -23,22 +23,22 @@ export class Logger implements LoggerService {
   }
 
   debug(message: string, context?: string): void {
-    this.logger.debug(`${context ? '[' + context + '] ' : ''}${message}`, context);
+    this.logger.debug(message, [context]);
   }
 
   error(message: string, trace?: string, context?: string): void {
-    this.logger.error(`${context ? '[' + context + '] ' : ''}${message}`, trace, context);
+    this.logger.error(message, [trace, context]);
   }
 
   log(message: string, context?: string): void {
-    this.logger.info(`${context ? '[' + context + '] ' : ''}${message}`, context);
+    this.logger.info(message, [context]);
   }
 
   verbose(message: string, context?: string): void {
-    this.logger.verbose(`${context ? '[' + context + '] ' : ''}${message}`, context);
+    this.logger.verbose(message, [context]);
   }
 
   warn(message: string, context?: string): void {
-    this.logger.warn(`${context ? '[' + context + '] ' : ''}${message}`, context);
+    this.logger.warn(message, [context]);
   }
 }
